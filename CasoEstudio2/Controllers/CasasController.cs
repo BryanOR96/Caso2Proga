e9b48338204a772748b9d79f942476191f37773a
BryanOR96 <bryorozco@gmail.com>
2024-08-21 00:00:51 -0600

    base del prorama

---
+++ CasoEstudio2/Controllers/CasasController.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using CasoEstudio2.BaseDatos;
+using CasoEstudio2.Models;
+
+namespace CasoEstudio2.Controllers
+{
+    public class CasasController : Controller
+    {
+        public CasasSistema db = new CasasSistema(); 
+        // GET: Casas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ConsultaCasas()
+        {
+            var casas = db.CasasSistema
+                 .Where(c => c.PrecioCasa >= 115000 && c.PrecioCasa <= 180000)
+                 .OrderBy(c => c.UsuarioAlquiler == null ? 0 : 1)
+                 .ToList();
+
+            return View(casas); ;
+        }
+
+        public ActionResult AlquilerCasas()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ CasoEstudio2/Models/CasasModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CasoEstudio2.Models
+{
+    public class CasasModel
+    {
+        [Key]
+        public long IdCasa { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string DescripcionCasa { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "El precio debe ser un valor positivo.")]
+        public decimal PrecioCasa { get; set; }
+
+        [StringLength(30)]
+        public string UsuarioAlquiler { get; set; }
+
+        public DateTime? FechaAlquiler { get; set; }
+
+        // Propiedad calculada para mostrar el estado de la casa (Disponible o Reservada)
+        public string Estado
+        {
+            get
+            {
+                return string.IsNullOrEmpty(UsuarioAlquiler) ? "Disponible" : "Reservada";
+            }
+        }
+    }
+}
